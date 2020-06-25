e9e6cfe78327ab3ae19b83a4b0092bb850d7eaa0
nfcaspers <noelcaspers04@gmail.com>
2020-06-25 13:32:06 +0200

    Verify that array is sorted

--- Sort.java
+++ Sort.java
@@ -4,10 +4,23 @@ import java.util.Arrays;
 public class Sort {
     public static void main(String[] args) {
         Random rand = new Random();
-        int[] unsortedArray = rand.ints(100, 0, 1000).toArray();
-        System.out.println("Unsorted: " + Arrays.toString(unsortedArray)+"\n");
-        int[] sortedArray = bubblesort(unsortedArray);
-        System.out.println(Arrays.toString(sortedArray));
+        int[] unsortedArray = rand.ints(25, 0, 1000).toArray();
+        printStatus("Bubblesort", bubblesort(unsortedArray));
+    }
+    public static void printStatus(String sortingAlgorithm, int[] arr) {
+        if(checkArraySorted(arr)) {
+            System.out.println(sortingAlgorithm + " was successful");
+        } else {
+            System.out.println(sortingAlgorithm + " was not successful");
+        }
+    }
+    public static boolean checkArraySorted(int[] arr) {
+        for(int i=0; i < arr.length-1; i++) {
+            if(arr[i] > arr[i+1]) {
+                return false;
+            }
+        }
+        return true;
     }
     public static int[] bubblesort(int[] arr) {
         for(int x=0; x < arr.length; x++) {
