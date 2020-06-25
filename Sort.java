3e1cf5d5974d2ccd1666bce5b3769b2f702c28c5
nfcaspers <noelcaspers04@gmail.com>
2020-06-25 13:13:32 +0200

    Implemented bubblesort

--- Sort.java
+++ Sort.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+import java.util.Arrays;
+
 public class Sort {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Random rand = new Random();
+        int[] unsortedArray = rand.ints(100, 0, 1000).toArray();
+        System.out.println("Unsorted: " + Arrays.toString(unsortedArray)+"\n");
+        int[] sortedArray = bubblesort(unsortedArray);
+        System.out.println(Arrays.toString(sortedArray));
+    }
+    public static int[] bubblesort(int[] arr) {
+        for(int x=0; x < arr.length; x++) {
+            for(int y=0; y < arr.length - 1; y++) {
+                if(arr[y] > arr[y + 1]) {
+                    int temp = arr[y];
+                    arr[y] = arr[y+1];
+                    arr[y+1] = temp;
+                }
+            }
+        }
+        return arr;
     }
 }
