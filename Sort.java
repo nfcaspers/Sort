4a0011e1fcf38b9d2692bff9c42bd5834d13e853
nfcaspers <noelcaspers04@gmail.com>
2020-06-25 14:36:16 +0200

    Cleanup

--- Sort.java
+++ Sort.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
@@ -24,8 +23,8 @@ public class Sort {
     }
     public static int[] bubblesort(int[] arr) {
         for(int x=0; x < arr.length; x++) {
-            for(int y=0; y < arr.length - 1; y++) {
-                if(arr[y] > arr[y + 1]) {
+            for(int y=0; y < arr.length-1; y++) {
+                if(arr[y] > arr[y+1]) {
                     int temp = arr[y];
                     arr[y] = arr[y+1];
                     arr[y+1] = temp;
